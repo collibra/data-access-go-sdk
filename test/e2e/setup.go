86b4b8f3bd6310953606b337f3c2c254a6aaf3f4
Yurii Simashkevych <yurii.simashkevych@collibra.com>
2025-10-28 15:20:30 +0100

    Update setup.go

--- test/e2e/setup.go
+++ test/e2e/setup.go
@@ -14,22 +14,22 @@ var (
 )
 
 func getEnv(key, defaultValue string) string {
-    if value := os.Getenv(key); value != "" {
-        return value
-    }
-    return defaultValue
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
 }
 
 func TestMain(m *testing.M) {
-    baseURL  := getEnv("COLLIBRA_BASE_URL", "https://access-governance-e2e-1.collibra.tech/accessGovernance")
-    userName := getEnv("COLLIBRA_USERNAME", "Admin")
-    password := getEnv("COLLIBRA_PASSWORD", "admin")
+	baseURL := getEnv("COLLIBRA_BASE_URL", "https://access-governance-e2e-1.collibra.tech/accessGovernance")
+	userName := getEnv("COLLIBRA_USERNAME", "Admin")
+	password := getEnv("COLLIBRA_PASSWORD", "admin")
 
-    client = collibra.NewClient(userName, password, baseURL)
-    if client == nil {
-        panic("Failed to create Collibra client")
-    }
-    ctx = context.Background()
+	client = collibra.NewClient(userName, password, baseURL)
+	if client == nil {
+		panic("Failed to create Collibra client")
+	}
+	ctx = context.Background()
 
-    os.Exit(m.Run())
+	os.Exit(m.Run())
 }
