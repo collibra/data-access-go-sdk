b050b6d9216d908ee5f27249c0a12276d0f9869e
Thomas Timmers <thomas.timmers@collibra.com>
2026-03-22 09:18:44 +0100

    Update test for using real expected user

--- services/user_service_test.go
+++ services/user_service_test.go
@@ -3,12 +3,14 @@ package services_test
 import (
 	"testing"
 
-	sdk "github.com/collibra/data-access-go-sdk"
-	"github.com/collibra/data-access-go-sdk/internal/schema"
-	"github.com/collibra/data-access-go-sdk/services"
-	"github.com/collibra/data-access-go-sdk/utils"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/suite"
+
+"github.com/google/uuid"
+"github.com/stretchr/testify/suite"
+
+sdk "github.com/collibra/data-access-go-sdk"
+"github.com/collibra/data-access-go-sdk/internal/schema"
+"github.com/collibra/data-access-go-sdk/services"
+"github.com/collibra/data-access-go-sdk/utils"
 )
 
 type UserServiceTestSuite struct {
@@ -64,7 +66,7 @@ func (suite *UserServiceTestSuite) TestGetCurrentUser() {
 
 	suite.NotEmpty(user.Id)
 
-	expectedName := "Admin Istrator"
+	expectedName := "Data Access Go SDK"
 	suite.Equal(expectedName, user.Name)
 
 	suite.NotNil(user.Email)
